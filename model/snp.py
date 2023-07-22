93d2a57ef031c262e11ff597e4edff2107359b58
jilin.li <1224935367@qq.com>
2023-07-22 07:02:51 +0000

    self-attention with snp v1.0

--- model/snp.py
+++ model/snp.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import numpy as np
+import math
 
 class SNPModule(nn.Module):
     def __init__(self, embed_size:int, forward_expansion: float, liner_sub_sample = 8, dropout:float = 0., *args, **kward) -> None:
@@ -15,20 +16,21 @@ class SNPModule(nn.Module):
         self.rho1 = nn.Sequential(
             nn.Linear(embed_size, embed_size//liner_sub_sample),
             nn.Tanh(),
-            nn.Dropout(dropout),
             nn.Linear(embed_size//liner_sub_sample, embed_size)
         )
 
         self.rho2 = nn.Sequential(
             nn.Linear(embed_size, embed_size//liner_sub_sample),
             nn.Tanh(),
-            nn.Dropout(dropout),
             nn.Linear(embed_size//liner_sub_sample, embed_size)
         )
 
         self.g = nn.Hardswish()
+        self.f = self.g
         self.dropout = nn.Dropout(dropout)
+        self.u = nn.Parameter(torch.Tensor(1, embed_size))
 
+    # x:torch.Size([4912, 144])  
     def forward(self, x, T = 0):
         self.zero_x = torch.zeros_like(x)
         # x.size: (b, n)
@@ -43,14 +45,72 @@ class SNPModule(nn.Module):
         forward = forward_fire + forward_not_fire
         return forward
 
+
+class SNPModule1(nn.Module):
+    def __init__(self, embed_size:int, forward_expansion: float, liner_sub_sample = 8, dropout:float = 0., *args, **kward) -> None:
+        super(SNPModule1, self).__init__(*args, **kward)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size),
+        )
+        self.snp_forward_fc1 = nn.Linear(embed_size, forward_expansion * embed_size)
+        self.snp_forward_fc2 = nn.Linear(forward_expansion * embed_size, embed_size)
+        self.rho1 = nn.Sequential(
+            nn.Linear(embed_size, embed_size),
+            # nn.Tanh(),
+            # nn.Linear(embed_size//liner_sub_sample, embed_size)
+        )
+
+        self.rho2 = nn.Sequential(
+            nn.Linear(embed_size, embed_size),
+            # nn.Tanh(),
+            # nn.Linear(embed_size//liner_sub_sample, embed_size)
+        )
+
+        self.g = nn.Softmax()
+        self.f = nn.Softmax()
+        self.dropout = nn.Dropout(dropout)
+        # self.u = nn.Parameter(torch.Tensor(1, embed_size))
+        self.u = None
+        self.embed_size = embed_size
+        # self.init_weight()
+
+    def init_weight(self):
+        stdv = 1.0 / math.sqrt(self.embed_size)
+        self.u.data.uniform_(-stdv, stdv)
+
+    # x:torch.Size([4912, 144])  
+    def forward(self, x, T = -math.inf):
+        with torch.no_grad():
+            if self.u is None:
+                self.u = torch.zeros_like(x).mean(0)
+        x = self.u + x
+        self.zero_x = torch.zeros_like(x)
+        nedd_fire_data = torch.where(x > T, x, self.zero_x)
+        # if x < T not fire
+        not_need_fire_data = torch.where(x < T, x, self.zero_x)
+        fired_data = nedd_fire_data - self.g(nedd_fire_data)
+        not_fired_data = not_need_fire_data
+        u = fired_data + not_fired_data
+
+        u = torch.mean(u, dim=0).reshape(1, -1)
+        h =  self.f(self.u + x)
+
+        with torch.no_grad():
+            # genarate function
+            self.u.data = u
+        return h
+
 class SNPAttention(nn.Module):
     def __init__(self, embed_size):
         super(SNPAttention, self).__init__()
-        self.snp = SNPModule(embed_size, 1)
+        self.snp = SNPModule1(embed_size, 1)
 
+    # Q,K,V:torch.size([16, 4, 307, 12, 16])
     def forward(self, Q, K, V):
         B, n_heads, len1, len2, d_k = Q.shape
-        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)
+        scores = torch.matmul(Q, K.transpose(-1, -2)) / np.sqrt(d_k)    # torch.size([16, 4, 307, 12, 12])
         # # scores : [batch_size, n_heads, T(Spatial) or N(Temporal), N(Spatial) or T(Temporal), N(Spatial) or T(Temporal)]
         # # scores.masked_fill_(attn_mask, -1e9) # Fills elements of self tensor with value where mask is True.
 
@@ -62,9 +122,10 @@ class SNPAttention(nn.Module):
             # batch size 307, vector_length, vector_length
             # batch_size * 307, vector_length * vector_length
             score = scores[:,i,...]
-            score = score.reshape([b*n, hidden_dim * hidden_dim])
-            attn_i = self.snp(score)
-            attn_i = attn_i.reshape(b,1,n, hidden_dim, hidden_dim)
+            score = score.reshape([b*n, hidden_dim * hidden_dim])  # torch.Size([4912, 144])
+            
+            attn_i = self.snp(score)   # torch.Size([4912, 144])
+            attn_i = attn_i.reshape(b,1,n, hidden_dim, hidden_dim)  # torch.Size([16, 1, 307, 12, 12])
             attns.append(attn_i)
         attn = torch.concat(attns, dim=1)
         context = torch.matmul(attn,
